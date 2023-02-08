126cf7c96fa4bad1b924325f200704689055f349
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-02-08 18:48:46 +0530

    header completed

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './Header.css'
-const header = () => {
+import logo from '../../assets/Logo.png'
+import vector from '../../assets/Red Vector.png'
+const Header = () => {
   return (
-    <div>
-      Header
+    <div className='food-blog__header'>
+      <div className="food-blog__header-content">
+        <div className="food-blog__header-content-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="food-blog__header-content-heading">
+
+        <h1>Discover the <span>Best</span> Food and Drinks</h1>
+        <p>Naturally made Healthcare Products for the better care &amp; support of your body.</p>
+        <button>Explore Now!</button>
+        </div>
+      </div>
+      <div className="food-blog__header-imgs">
+        <button>Get In Touch</button>
+        <img src={vector} alt="vector" />
+      </div>
     </div>
   )
 }
 
-export default header
+export default Header
