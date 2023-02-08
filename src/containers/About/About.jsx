be61d3c9124ac60f520723d2878c8d384c1a39c1
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-02-08 20:45:13 +0530

    about section complete

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './About.css'
+import aboutUs from '../../assets/AboutUs.png'
 const About = () => {
   return (
-    <div>
-      About us
+    <div className='food-blog__about'>
+      <div className="food-blog__about-img">
+        <img src={aboutUs} alt="About Us" />
+      </div>
+      <div className="food-blog__about-content">
+        <h2>About Us</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.</p>
+        <button>Read More</button>
+      </div>
     </div>
   )
 }
