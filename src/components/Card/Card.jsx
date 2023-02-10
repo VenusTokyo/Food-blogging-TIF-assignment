9f2583af4da5ea5a2167770996fb24d6ef06e48a
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-02-10 17:44:21 +0530

    send props to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Card.css'
-import card1 from '../../assets/card1.png'
-const Card = () => {
+// import card1 from '../../assets/card1.png'
+const Card = (props) => {
   return (
     <div className='food-blog__card'>
       <div className="food-blog__card-img">
-        <img src={card1} alt="" />
+        <img src={props.img} alt="" />
       </div>
       <div className="food-blog__card-content">
-        <h3 className='food-blog__card-heading'>Grilled  Tomatoes at Home</h3>
+        <h3 className='food-blog__card-heading'>{props.title}</h3>
         <p className='food-blog__card-description'>PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...</p>
         <button>Read More</button>
       </div>
--- src/containers/Articles/Articles.jsx
+++ src/containers/Articles/Articles.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Articles.css'
 import Card from '../../components/Card/Card'
+import img1 from '../../assets/card1.png'
+import img2 from '../../assets/card2.png'
+import img3 from '../../assets/card3.png'
 const Articles = () => {
   return (
     <div className='food-blog__articles'>
@@ -8,12 +11,9 @@ const Articles = () => {
         <h2>Latest Articles</h2>
       </div>
       <div className="food-blog__articles-card-container">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card img={img1} title={"Grilled  Tomatoes at Home"}/>
+      <Card img={img2} title={"Snacks for Travel"}/>
+      <Card img={img3} title={"Post-workout Recipes"}/>
       </div>
     </div>
   )
